1f6f7daa5c1908af2de5e05edce6ad253f7e25e0
cutebid <cutebid1993@gmail.com>
2024-10-12 13:28:29 +0100

    made some changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,45 @@ import Regulation from "@/components/Regulation";
 import Beneficiaries from "@/components/Beneficiaries";
 import Community from "@/components/Community";
 import DownloadSection from "@/components/DownloadSection";
-import { investmentData } from "../static/data";
+
 import HeroSection from "@/components/HeroSection";
 
 const HomePage = () => {
+
+   const investmentData = [
+    {
+      title: "Invest your money in dollars",
+      mobileTitle: "Superior Performance",
+      info: "By holding your investments in a stable currency, your money grows more over time and retains its value better.",
+      imgSrc: "/file1.png",
+      flexDirection: "md:flex-row",
+    },
+    {
+      title: "Choose what's best for you",
+      mobileTitle: "Personalization",
+      info: "Unlike other platforms, Rise lets you pick between stocks, US real estate, and fixed income, according to your risk appetite.",
+      imgSrc: "/file2.png",
+      flexDirection: "md:flex-row-reverse",
+    },
+    {
+      title: "Set goals and reach them",
+      mobileTitle: "Diversification",
+      info: "You can invest towards a goal on Rise--retirement, higher education, save for your home or travel budgets.",
+      imgSrc: "/file3.png",
+      flexDirection: "md:flex-row",
+    },
+    {
+      title: "We remember so you don’t have to",
+      mobileTitle: "Consistency",
+      info: "Our Auto-invest feature makes it easy to stay consistent. Set a funding amount and Rise will automatically fund your investment.",
+      imgSrc: "/file4.png",
+      flexDirection: "md:flex-row-reverse",
+    },
+  ];
+  
   return (
     <main className="relative  overflow-hidden pt-36">
-      <HeroSection />
+     <HeroSection/>
       <section className="mt-5 lg:mt-0">
         {investmentData.map((investment, index) => (
           <ChooseInvestment key={index} {...investment} />
